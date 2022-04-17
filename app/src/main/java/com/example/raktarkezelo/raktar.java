9b971b3da6bf94bbbd81c743bb673c196fc549ce
Balázs Bélteki <pcgyerek465@gmail.com>
2022-04-17 14:41:44 +0200

    ismét újragondolva az egész

---
+++ app/src/main/java/com/example/raktarkezelo/raktar.java
@@ -0,0 +1,150 @@
+package com.example.raktarkezelo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class raktar extends AppCompatActivity {
+ListView listView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.raktar:
+                setContentView(R.layout.activity_main);
+                break;
+            case R.id.termek:
+                setContentView(R.layout.activity_main_2);
+                break;
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+        getJSON("http://192.168.0.80/storager/raktar.php");
+
+    }
+    private void getJSON(final String urlWebService) {
+
+
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+
+
+
+                try {
+                    URL url = new URL(urlWebService);
+
+
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+
+                    StringBuilder sb = new StringBuilder();
+
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+
+                    String json;
+
+
+                    while ((json = bufferedReader.readLine()) != null) {
+
+
+                        sb.append(json + "\n");
+                    }
+
+
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
+
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+
+
+
+
+    }
+
+    private void loadIntoListView(String json) throws JSONException {
+        JSONArray jsonArray = new JSONArray(json);
+
+        String[] storages = new String[jsonArray.length()];
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+
+            JSONObject obj = jsonArray.getJSONObject(i);
+
+            storages[i] = obj.getString("name");
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, storages);
+
+        listView.setAdapter(arrayAdapter);
+
+
+    }
+
+
+}
+
